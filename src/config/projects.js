049c0650f0a9322e69167ee999ffc9659a4a792e
mani-s12g <mani.s12g@gmail.com>
2025-08-21 19:33:13 +0530

    cursor, theme, routing

---
+++ src/config/projects.js
@@ -0,0 +1,25 @@
+const projectDetails = {
+  keywords: ["Work", "Hobby", "Playfield"],
+  intro:
+    "I'm obsessed with creating things that really moves me, and which are unique",
+  details: [
+    {
+      title: "Basil Resort",
+      description: "",
+      tech: "HTML, CSS, JS, React JS, Redux, GSAP Animations, MongoDB, AWS S3, AWS EC2 Instance for server",
+      myRole: "",
+      challenges: "",
+      outcome: "",
+    },
+    {
+      title: "Aavelona",
+      description: "",
+      tech: "HTML, CSS, JS, React JS, Redux, GSAP Animations, MongoDB, AWS S3, AWS EC2 Instance for server",
+      myRole: "",
+      challenges: "",
+      outcome: "",
+    },
+  ],
+};
+
+export default projectDetails;
--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
-
-document.addEventListener('DOMContentLoaded', () => {
-    const interBubble = document.querySelector('.interactive');
-    let curX = 0;
-    let curY = 0;
-    let tgX = 0;
-    let tgY = 0;
-
-    function move() {
-        curX += (tgX - curX) / 20;
-        curY += (tgY - curY) / 20;
-        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
-        requestAnimationFrame(() => {
-            move();
-        });
-    }
-
-    window.addEventListener('mousemove', (event) => {
-        tgX = event.clientX;
-        tgY = event.clientY;
-    });
-
-    move();
-});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
