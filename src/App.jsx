5f828e1ec95526bf80fed462fc10ed33fa93a935
mani-s12g <mani.s12g@gmail.com>
2025-08-24 17:43:27 +0530

    all

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import {} from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CursorBall from "./components/CursorBall/CursorBall";
 import Home from "./pages/Home/Home";
-import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
 import Projects from "./pages/Projects/Projects";
-// import Skills from "./pages/Skills/Skills"
+import Skills from "./pages/Skills/Skills"
 import "./assets/styles/global.css";
 
 function App() {
@@ -15,14 +15,19 @@ function App() {
     <>
       <CursorBall />
       <Router>
-        <Navbar />
+        {/* header */}
+        <Header />
+        
+        {/* routes */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          {/* <Route path="/skills" element={<Skills />} /> */}
+          <Route path="/skills" element={<Skills />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
+
+        {/* footer */}
         <Footer />
       </Router>
     </>
--- src/sections/Main/Main.jsx
+++ src/sections/Main/Main.jsx
@@ -3,20 +3,37 @@ import styles from "./Main.module.css";
 
 function Main() {
   return (
-    <main className={styles.container}>
-      <h1 className={styles.heading1}>
-        <span className="cursor-grow">Manikanta</span>
-      </h1>
-      <h2 className={styles.heading2}>
+    <main className={`${styles.mainStyle} container`}>
+      <div className={styles.headDiv}>
+        <div>
+          <h1 className={styles.heading1}>
+            <span className="cursor-grow">Manikanta</span>
+          </h1>
+        </div>
+        <div>
+          <p>
+            <strong>
+              <span className="cursor-grow">Front-End Developer</span>
+            </strong>
+            <br />
+            <small>Worked @ Liquid Lab | Infobell IT Pvt Ltd </small>
+          </p>
+        </div>
+        {/* <h2 className={styles.heading2}>
         <span className="cursor-grow">Front-End Developer</span>
-      </h2>
-      <h4 className={styles.heading4}>
-        Worked @ Liquid Lab | Infobell IT Pvt Ltd
-      </h4>
-      <h5 className={styles.heading5}>Good @ Web Technologies</h5>
-      <button className={styles.viewBtn}>
-        <span className="cursor-grow">View my work</span>
-      </button>
+        </h2>
+        <h4 className={styles.heading4}>
+          Worked @ Liquid Lab | Infobell IT Pvt Ltd
+        </h4> */}
+        <div>
+          <h5 className={styles.heading5}>Good @ Web Technologies</h5>
+          <br />
+          <br />
+          <button className={styles.viewBtn}>
+            <span className="cursor-grow">View my work</span>
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
