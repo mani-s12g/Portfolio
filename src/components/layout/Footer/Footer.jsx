e8f087f7d6236e57f81c7519286c776cd64ec1e0
mani-s12g <mani.s12g@gmail.com>
2025-09-06 21:56:46 +0530

    footerchanges

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -7,13 +7,35 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 function Footer() {
   return (
     <footer className={styles.footer}>
+      {/* <small>
+        Loosely designed and coded in Visual Studio Code by yours truly. Built
+        with React.js and CSS, deployed with github pages. Typography combines
+        multiple typefaces for hierarchy and style.
+      </small> */}
+
+      <div className={styles.footerCredits}>
+        <p>
+          Designed & coded with in <strong>Visual Studio Code</strong> by yours
+          truly.
+        </p>
+        <p>
+          Built with ❤️ & Passion not just code, using <strong>React.js</strong>{" "}
+          and <strong>CSS</strong>, deployed on <strong>GitHub Pages</strong>.
+        </p>
+      </div>
+      <div className={styles.findMe}>
+        You know how to find me
+        <br />
+        <strong>mani.s12g@gmail.com</strong>
+      </div>
       <ul>
         <li
           className={styles.email}
-          onClick={() => window.open(window.location.href = "mailto:mani.s12g@gmail.com")}
+          // onClick={() => window.open(window.location.href = "mailto:mani.s12g@gmail.com")}
         >
-          <span className={styles.text}>email</span>
-          <EmailOutlinedIcon className={styles.icon} />
+          {/* <span className={styles.findMe}>& these -</span> */}
+          {/* <span className={styles.text}>mani.s12g@gmail.com</span> */}
+          {/* <EmailOutlinedIcon className={styles.icon} /> */}
         </li>
 
         <li
